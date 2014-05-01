31fa05582d4c327df194c8cf97bc2478a45af12b
activ <jakub.sacha@vert.pl>
2014-05-01 11:14:44 +0200

    Initial commit

---
+++ src/Jakubsacha/Adminlte/AdminlteServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Jakubsacha\Adminlte;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminlteServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jakubsacha/adminlte');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
