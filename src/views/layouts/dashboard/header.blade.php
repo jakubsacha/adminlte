67cf70bd96b843679bf641b9f959a060694d8f79
activ <jakub.sacha@vert.pl>
2014-05-09 16:01:52 +0200

    fix admin name

--- src/views/layouts/dashboard/header.blade.php
+++ src/views/layouts/dashboard/header.blade.php
@@ -1,7 +1,6 @@
 <header class="header">
     <a href="{{ URL::route('indexDashboard') }}" class="logo">
-        <!-- Add the class icon to your logo image or logo icon to add the margining -->
-        Admin
+        {{$siteName}}
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
